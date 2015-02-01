45ac29a5237839aff57490bcf492ad86d4f1dec9
Eric Moyer <eric@lemoncrab.com>
2015-01-31 23:38:23 -0800

    Alpha Build 1

---
+++ js/drifter.js
@@ -0,0 +1,70 @@
+var DrifterFallSpeed = 0.3;
+
+var Drifter = Polygon.extend({
+
+	init: function(p, s, x, y, radius, radialAngle, color, f_radiusToAngularVelocity){
+		this._super(p, color);
+
+		this.center_x = x;
+		this.center_y = y;
+		this.radius = radius;
+		this.radialAngle = radialAngle;
+		this.angle = 0;
+		this.scale = s;
+
+		this.x = null;
+		this.y = null;
+
+		this.setScale(s);
+		this.radial_to_cardinal();
+		this.alive = true;
+		this.f_radiusToAngularVelocity = f_radiusToAngularVelocity;
+
+		this.die_sound = new Howl({
+			urls: ['sounds/Drifterexplosion.wav'],
+			volume: 0.25,
+		});
+
+	},
+
+	// Calculate caridnal position and angle from radial position and angle
+	radial_to_cardinal: function(){
+		this.setAngle(this.radialAngle - Math.PI/2);
+		this.x = this.center_x + this.radius * Math.cos(this.radialAngle);
+		this.y = this.center_y + this.radius * Math.sin(this.radialAngle);
+	},
+
+	collide: function(aster){
+		if (!this.visible){
+			return false;
+		}
+		for(i=0, len=this.points.length -2; i<len; i+=2){
+			var x = this.points[i] + this.x;
+			var y = this.points[i+1] + this.y;
+
+			if (aster.hasPoint(x,y)){
+				return true;
+			}
+		}
+		return false;
+	},
+
+	hasPoint: function(x, y) {
+		return this._super(this.x, this.y, x, y);
+	},
+
+	update: function(paceFactor) {
+		// Add rotational angle to stars based on radius
+		this.radialAngle += this.f_radiusToAngularVelocity(this.radius);
+		this.radius -= DrifterFallSpeed;
+		this.radial_to_cardinal();
+		if (this.radius < 30){
+			// Drifter fell into the vortex, so regenerate it at the outside
+			this.alive = false;
+		}
+	},
+
+	draw: function(ctx){
+		ctx.drawPolygon(this, this.x, this.y);
+	}
+})
